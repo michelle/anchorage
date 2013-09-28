4e40207256d9e6f1e3b0bc28543b35d2f98b5f0b
ericz <really.ez@gmail.com>
2013-09-28 15:23:30 -0700

    Add index.js to manage game operations

---
+++ cards/js/index.js
@@ -0,0 +1,11 @@
+﻿var game = new Game();
+function test () {
+  game.join(); game.join(); game.join(); game.join(); game.start(); game.dumpHands();
+
+
+}
+
+
+function startGame() {
+  game.start();
+}
