f974fe4387a19aea6962af14459f9ee4c5cffd40
ericz <really.ez@gmail.com>
2013-09-28 17:11:22 -0700

    duplicate

--- cards/js/index.js
+++ cards/js/index.js
@@ -2,7 +2,7 @@
 $(function(){
 
 
-  var display = new Display();
+  display = new Display();
 
   var events = ['join', 'game-start', 'round-start', 'turn-start', 'turn-end', 'round-end', 'game-end'];
   async.each(events, function(event){
